f5cfebe4fe43b21ed6c961efe1b282d1776c1c52
WM-Acessibilidade <leonardofilho@ymail.com>
2026-04-21 19:57:12 -0300

    Delete live directory

--- live/livekit-config.js
+++
@@ -1,5 +0,0 @@
-window.AUDESC_CONFIG = {
-  livekitUrl: "wss://audesc-live-dh77x30o.livekit.cloud",
-  tokenEndpoint: "https://audesc-livekit-server.onrender.com/token",
-  bundleUrl: "./livekit-client.umd.min.js"
-};
