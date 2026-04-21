9b356144e10d55bfe3f88e3ee9e1561ce6731fed
WM-Acessibilidade <leonardofilho@ymail.com>
2026-04-21 11:21:35 -0300

    Add files via upload

---
+++ live/livekit-config.js
@@ -0,0 +1,5 @@
+window.AUDESC_CONFIG = {
+  livekitUrl: "wss://audesc-live-dh77x30o.livekit.cloud",
+  tokenEndpoint: "https://audesc-livekit-server.onrender.com/token",
+  bundleUrl: "https://unpkg.com/livekit-client/dist/livekit-client.umd.min.js"
+};
